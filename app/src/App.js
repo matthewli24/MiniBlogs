9e0a06ac799ecf99972e9401573118243dce3695
Matthew Li <matthewli2491@gmail.com>
2019-11-19 22:44:35 -0500

    set up react router and so expected components

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Me</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <h1>Hello Me</h1>
+        </div>
+      </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
